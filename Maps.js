968ed6ea0d3429cd0afc7c1c15e207ee507ca579
rimadas123 <35536940+rimadas123@users.noreply.github.com>
2020-01-06 19:11:40 +0530

    Update Maps.js

--- Maps.js
+++ Maps.js
@@ -1,1 +1,17 @@
+<script>
+  //Data sets of students
+  
+  var students = [
+   { rollNo: 21, name: 'Alpha' }, 
+	{ rollNo: 22, name: 'Beta' }, 
+	{ rollNo: 23, name: 'Gamma' }, 
+	{ rollNo: 24, name: 'Delta' }, 
+	{ rollNo: 25, name: 'Omega'} 
+  ];
 
+var TotalStudent = students.map(function(student){
+  return student.rollno;
+}));
+
+console.log(TotalStudent);
+</script>
